4cf82c881b95fdd2afeb9296ec3a3cea1c434285
cosmos666 <sergey.demyanosov@gmail.com>
2021-05-27 19:27:11 +0300

    вава

--- Clean_proj/qt_calss.py
+++ Clean_proj/qt_calss.py
@@ -4,29 +4,33 @@
 """
 import pandas as pd
 import numpy as np
-
+import logging
 import sys, time
 from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtCore import QThread
 from PyQt5.QtWidgets import QApplication, QDialog, QMessageBox, QComboBox
 
 
-
-
 class TryThresd(QThread):
      change_value = QtCore.pyqtSignal(object)
+
+
 
      def __init__(self,root,sp_name,nam,col,skipr=None):
      # def __init__(self, root): #skiprows=None
         super().__init__(root)
-        # print('test ' + sp_name)
+        logger = logging.getLogger("Модуль считывания поток")
+        logger.info("{} запустился инит на считывание информации".format(sp_name))
         self.main = root
         self._sp_name=sp_name
         self._nam=nam
         self._col=col
         self._skipr=skipr
+        logger.info(" %s инит считался" % (self._sp_name))
 
      def run(self):
+         logger = logging.getLogger("Модуль запуск потока")
+         logger.info(" %s Запустился в поток" % (self._sp_name))
          if self.main.file_dict[self._sp_name]=="":
              self.change_value.emit('')
              self.close()
@@ -38,10 +42,15 @@ class TryThresd(QThread):
                                skiprows=self._skipr)
          except Exception:
            e = sys.exc_info()[1]
-           print(e.args[0])
+           logger.exception(e.args[0])
 
          self.main.CurStatus.addItem(self._sp_name +" приехал")
-         self.change_value.emit([self._sp_name,df_zak])
+         logger.info(" %s Отработал поток" % (self._sp_name))
+
+         
+         self.change_value.emit([self._sp_name,
+                                 df_zak])
+
 
 
 
